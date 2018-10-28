8f1d119acca90a4e0975197c6d444b0049940610
suark <suark.nairb@gmail.com>
2018-10-28 17:19:59 -0600

    working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
-// import now from 'performance-now'
+import now from 'performance-now'
 import { ComponentType } from 'react'
+interface objectWithStringIndex {
+  [key:string] : any
+}
 
-function RNProfiler(component: ComponentType) {
-  const componentName = component.displayName
+function RNProfiler(component: ComponentType & objectWithStringIndex) {
+  const componentName = component.displayName || 'Component'
   console.log(`Profiling: ${componentName}`)
-  const names = Object.getOwnPropertyNames(Object.getPrototypeOf(component))
-  names.forEach((func) => {
-    
+
+  const ownPropertyNames = Object.getOwnPropertyNames(Object.getPrototypeOf(component))
+  ownPropertyNames.forEach((propertyName) => {
+    const property = component[propertyName]
+    if (typeof  property === 'function') {
+      component[propertyName] = function () {
+        const start = now()
+        const result = property.apply(component, arguments)
+        const difference = now() - start
+        console.log(`${componentName}: ${propertyName}: ${difference.toFixed(3)}ms`)
+        return result
+      }
+    }
   })
 }
 
